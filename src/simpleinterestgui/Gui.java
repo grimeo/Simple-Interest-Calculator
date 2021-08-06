25ceed677ad0293a9b019b2b5dd22882487ebeb8
doggo <kilazlove@gmail.com>
2021-08-07 03:15:56 +0800

    create JFrame

--- src/simpleinterestgui/Gui.java
+++ src/simpleinterestgui/Gui.java
@@ -1,9 +1,21 @@
 package simpleinterestgui;
 
+import javax.swing.JFrame;
+
 public class Gui {
     
     Gui(){
+        JFrame frame = new JFrame("Simple Interest");
         
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        
+        
+        
+        
+        frame.setVisible(true);
     }
     
 }
