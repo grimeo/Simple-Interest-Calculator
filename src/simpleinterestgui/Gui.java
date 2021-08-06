f4c884040e7f9321a25180a54d85569407aabfb6
doggo <kilazlove@gmail.com>
2021-08-07 03:22:18 +0800

    create panel

--- src/simpleinterestgui/Gui.java
+++ src/simpleinterestgui/Gui.java
@@ -1,18 +1,24 @@
 package simpleinterestgui;
 
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Gui {
     
     Gui(){
         JFrame frame = new JFrame("Simple Interest");
+        JPanel panel = new JPanel();
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
         frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setSize(500, 500);
         frame.setLocationRelativeTo(null);
         
-        
+        panel.setSize(500,500);
+        panel.setBackground(Color.decode("#d3d3d3"));
+        frame.add(panel);
         
         
         frame.setVisible(true);
