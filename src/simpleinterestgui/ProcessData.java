a50a53fd943db662c3ae74d550f373bfec60a398
doggo <kilazlove@gmail.com>
2021-08-07 20:11:34 +0800

    create strategy pattern

--- src/simpleinterestgui/ProcessData.java
+++ src/simpleinterestgui/ProcessData.java
@@ -8,3 +8,30 @@ public class ProcessData {
 interface SolveProblem{
     public void solve();
 }
+
+class SolveInterest implements SolveProblem{
+
+    @Override
+    public void solve() {
+        
+    }
+    
+}
+
+class SolveRate implements SolveProblem{
+
+    @Override
+    public void solve() {
+        
+    }
+    
+}
+
+class SolvePrincipal implements SolveProblem{
+
+    @Override
+    public void solve() {
+        
+    }
+    
+}
